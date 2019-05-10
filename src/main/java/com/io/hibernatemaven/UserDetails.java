2836253913a927e69d45c017a91f6153288b416e
bhawna budhiraja <bhawna.bhawna3@soprasteria.com>
2019-05-10 15:56:31 +0530

    added

--- src/main/java/com/io/hibernatemaven/UserDetails.java
+++ src/main/java/com/io/hibernatemaven/UserDetails.java
@@ -4,6 +4,9 @@ import java.util.Date;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.persistence.Transient;
 
 /*@table annotation for changing the table name but the entity name will remain same*/
@@ -27,7 +30,10 @@ public class UserDetails {
 	//@Transient:-indicates that field will not be stored in the table.
 	@Transient
 	private String name;
+	//@Lob:-Used for large java objects.with this annotation,the value in userdesc will be stored in numbers.
+	@Lob
 	private String userDescription;
+	@Temporal(TemporalType.TIME)
 	private Date joinedDate;
 
 	public int getId() {
