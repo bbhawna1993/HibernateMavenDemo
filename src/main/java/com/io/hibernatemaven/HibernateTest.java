d772ae5c3f4db9fb30be0e9db71da6e8fbb79d6f
bhawna budhiraja <bhawna.bhawna3@soprasteria.com>
2019-05-10 15:08:21 +0530

    added

--- src/main/java/com/io/hibernatemaven/HibernateTest.java
+++ src/main/java/com/io/hibernatemaven/HibernateTest.java
@@ -1,5 +1,7 @@
 package com.io.hibernatemaven;
 
+import java.util.Date;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -9,8 +11,10 @@ public class HibernateTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		UserDetails user=new UserDetails();
-		user.setId(5);
-		user.setName("bhawna5");
+		user.setId(1);
+		user.setName("first user");
+		user.setJoinedDate(new Date());
+		user.setUserDescription("user description");
 
 		SessionFactory sessionFactory=new Configuration().configure().buildSessionFactory();
 		Session session=sessionFactory.openSession();
--- src/main/java/com/io/hibernatemaven/UserDetails.java
+++ src/main/java/com/io/hibernatemaven/UserDetails.java
@@ -1,16 +1,34 @@
 package com.io.hibernatemaven;
 
-import javax.persistence.Column;
+import java.util.Date;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Transient;
 
-@Entity (name="USER_DETAILS")
+/*@table annotation for changing the table name but the entity name will remain same*/
+@Entity
 public class UserDetails {
 
+	public String getUserDescription() {
+		return userDescription;
+	}
+	public void setUserDescription(String userDescription) {
+		this.userDescription = userDescription;
+	}
+	public Date getJoinedDate() {
+		return joinedDate;
+	}
+	public void setJoinedDate(Date joinedDate) {
+		this.joinedDate = joinedDate;
+	}
 	@Id
 	private int id;
-	@Column(name="USER_NAME")
+	//@Transient:-indicates that field will not be stored in the table.
+	@Transient
 	private String name;
+	private String userDescription;
+	private Date joinedDate;
 
 	public int getId() {
 		return id;
